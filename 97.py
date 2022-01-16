135d006c3eabd056735dfc7c173de30afd6dcd2c
ArafAkter <79374814+ArafAkter@users.noreply.github.com>
2022-01-16 11:28:01 -0500

    Add files via upload

---
+++ 97.py
@@ -0,0 +1,133 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+print("guess a number 1 - 9")
+guess a number 1 - 9
+input()
+
+''
+
+guessedanswer=input()
+
+print(guessedanswer)
+
+print(input(whats the number between 1 and 9))
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print("whats the number between 1 and 9",input(whats the number between 1 and 9))
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print("whats the number between 1 and 9",input(Whats the number between 1 and 9))
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+input(whats the number between 1 and 9)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+input("whats the number between 1 and 9")
+whats the number between 1 and 9 1
+' 1'
+answer = input("whats the number between 1 and 9")
+whats the number between 1 and 9 1
+
+
+if(answer = 9):
+    
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+if(answer == 9):
+    print("You Win")
+answer = "whats the number between 1 and 9"
+SyntaxError: invalid syntax
+"whats the number between 1 and 9"
+'whats the number between 1 and 9'
+input(answer)
+ 19
+'9'
+
+
+
+if(answer == 9):
+    print("You Win")
+
+    
+
+
+if(answer == 9):
+    print("You Win")
+    else:
+        
+SyntaxError: invalid syntax
+if(answer == 9):
+    print("You Win")
+else:
+    print("You lose")
+input(answer)
+SyntaxError: invalid syntax
+input(answer)
+ 1
+''
+if(answer == 1):
+    print("You Win")
+else:
+    print("You lose")
+input(answer)
+SyntaxError: invalid syntax
+input(answer)
+ 1
+''
+'9'
+'9'
+
+input(answer)
+ 1
+''
+'1'
+'1'
+input(answer)
+ 1
+''
+if(answer < 8):
+    print("you win")
+else:
+    print("You lose")
+input(answer)
+SyntaxError: invalid syntax
+input(answer)
+ 1
+''
+'9'
+'9'
+input(answer)input(answer)
+SyntaxError: invalid syntax
+guesses = "what is the number from 1 - 9"
+input(guesses)
+what is the number from 1 - 99
+'9'
+if(answer < 8):
+    print("you win")
+else:
+    print("You lose")
+
+    
+Traceback (most recent call last):
+  File "<pyshell#53>", line 1, in <module>
+    if(answer < 8):
+TypeError: '<' not supported between instances of 'str' and 'int'
+guesses1 = "what is the number from 1 - 9"
+input(int(guesses1))
+Traceback (most recent call last):
+  File "<pyshell#55>", line 1, in <module>
+    input(int(guesses1))
+ValueError: invalid literal for int() with base 10: 'what is the number from 1 - 9'
+input(guesses1)
+what is the number from 1 - 99
+'9'
+print(int(guesses))
+Traceback (most recent call last):
+  File "<pyshell#57>", line 1, in <module>
+    print(int(guesses))
+ValueError: invalid literal for int() with base 10: 'what is the number from 1 - 9'
+if(guesses1 < 8):
+    print("you win")
+else:
+    print("You lose")
+
+    
+Traceback (most recent call last):
+  File "<pyshell#59>", line 1, in <module>
+    if(guesses1 < 8):
+TypeError: '<' not supported between instances of 'str' and 'int'
